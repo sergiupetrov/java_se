a04a2dfa56e82bc628e0f0eb40cc4bad983eb3d6
Serghei Petrov <serghei.gh.petrov@gmail.com>
2020-01-14 21:51:18 +0200

    practice 14.1.6

---
+++ src/MavenProj/practice_14.1.6/src/main/java/Age.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Age {
+    public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(1993, 4, 12);
+        System.out.println("My birthday:" + birthday);
+        LocalDate dateNow = LocalDate.now();
+        Period age = Period.between(birthday, dateNow);
+        System.out.println("Age: " + age);
+        System.out.println("Formatted: " + age.getYears() + " years " + age.getMonths() + " months " + age.getDays() + " days");
+    }
+}
