3ae978d6c33dbb704611fd44bc40d6f42161ae92
Serghei Petrov <serghei.gh.petrov@gmail.com>
2020-01-18 13:04:00 +0200

    practice 4.2 solution

---
+++ src/MavenProj/practice_04.02_numbers/src/main/java/NumbersClass.java
@@ -0,0 +1,11 @@
+public class NumbersClass {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.3;
+        double c = 3.14785;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
