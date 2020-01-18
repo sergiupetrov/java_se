c083498647619dc91e5e450729785edce16448eb
Serghei Petrov <serghei.gh.petrov@gmail.com>
2020-01-18 12:56:08 +0200

    practice 4.1 solution

---
+++ src/MavenProj/practice_04.01_boolean/src/main/java/BoolClass.java
@@ -0,0 +1,8 @@
+public class BoolClass {
+    public static void main(String[] args) {
+        Boolean b = true;
+        System.out.println("Initial boolean var: " + b);
+        b = false;
+        System.out.println("Changed value of boolean var: " + b);
+    }
+}
