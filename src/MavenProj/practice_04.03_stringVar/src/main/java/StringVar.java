5e43943090ec7297be2c63d4256ac51718d93440
Serghei Petrov <serghei.gh.petrov@gmail.com>
2020-01-18 13:16:37 +0200

    practice 4.3 solution

---
+++ src/MavenProj/practice_04.03_stringVar/src/main/java/StringVar.java
@@ -0,0 +1,10 @@
+public class StringVar {
+    public static void main(String[] args) {
+        String name = "Serghei";
+        String surname = "Petrov";
+        String message;
+
+        message = "My name is " + name + " " + surname + ".";
+        System.out.println("The message is: " + message);
+    }
+}
